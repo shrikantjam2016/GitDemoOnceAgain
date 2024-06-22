630efb055d0d48fade16b96bd99bf4bf8c3ff066
shri <shrikantjam2016@gmail.com>
2024-06-22 12:59:41 +0530

    Archi change in develop branch

---
+++ src/test/java/supportedAttributes/TestClass3.java
@@ -0,0 +1,26 @@
+package supportedAttributes;
+
+import org.testng.annotations.Test;
+
+public class TestClass3 {
+	
+	@Test 
+	public void devTest1() 
+	{ 
+	   System.out.println("Second Test Method class2"); 
+	   
+	   System.out.println("First change"); 
+	   
+	   System.out.println("Second change"); 
+	  }
+	
+	public void devTest2() 
+	{ 
+	   System.out.println("Third status changed "); 
+	   
+	   System.out.println("Third First change"); 
+	   
+	   System.out.println("Third Second change"); 
+	  }
+
+}
